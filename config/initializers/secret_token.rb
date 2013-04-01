321d2d097fd558734aa83f8618271a3d68334390
João Paulo <jpjcjbr@gmail.com>
2013-04-01 20:49:56 -0300

    versao inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardapioUnifesp::Application.config.secret_token = '7de0c3b38b8e6236e4849f90da1a4ad483d3ebe6b52b427fb03f213c4faafcc705ae3bd20d82a6175687d799cb6a066e30472700329bbb9b3b2b203382fa3895'
